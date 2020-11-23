7c874963006fed0a35de53fbf68e856e77587d86
Maxim Zhiburt <zhiburt@gmail.com>
2020-11-23 23:49:06 +0300

    Add license header to source files

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,7 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // todo: the main function and this module still looks freaky and not in complete state.
 //      at least `.unwrap()`
 //
 // todo: error handling?
+
 use crate::filters::Filter;
 use clap::Clap;
 use regex::RegexSet;
--- src/engine.rs
+++ src/engine.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::filters::Filter;
 use crate::searcher::{self, SearchResult, Searcher};
 use crate::shed::{Job, Sheduler};
